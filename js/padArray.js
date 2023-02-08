771eb3f75f0b423a49a8ec7f9bbd985d31102ada
mickey <shoey25ms@gmail.com>
2023-02-08 14:06:13 -0500

    Answer

--- js/padArray.js
+++ js/padArray.js
@@ -1,6 +1,13 @@
 // REMEMBER TO PSEUDOCODE
 const pad = (array, minSize, value=null) => {
-    // code goes here : ) 
+    if (array.length >= minSize) {
+        return array
+    }
+    while (array.length < minSize) {
+        array.push(value)
+    }
+    return array
+    
 }
 
 exports.pad = pad;
--- python/pad_array.py
+++ python/pad_array.py
@@ -1,3 +1,7 @@
 #REMEMBER TO PSEUDOCODE
 def pad(array, min_size, value = None):
-    pass 
+  if len(array) >= min_size:
+    return array
+  while len(array) < min_size:
+    array.append(value)
+  return array
